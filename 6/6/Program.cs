919daeab04dd0b854a921e2580e172dad62bf4c8
Alex Dragomir <alex.dragomir@caphyon.com>
2020-12-06 09:25:43 +0200

    6

---
+++ 6/6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace day2
+{
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+      string content = File.ReadAllText("TextFile1.txt");
+      Console.WriteLine(content.Split("\r\n\r\n").Sum(g => new HashSet<char>(g.Replace("\r\n", "")).Count()));
+      Console.WriteLine(content.Split("\r\n\r\n").Sum(g => g.Split("\r\n").Aggregate((r, g1) => new string(r.Intersect(g1).ToArray())).Count() ));
+    }
+  }
+}
