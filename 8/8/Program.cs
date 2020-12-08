09fe80f21eca7ace43f71db8f1c8c4a8f4047f59
Alex Dragomir <alex.dragomir@caphyon.com>
2020-12-08 09:17:52 +0200

    8

---
+++ 8/8/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace day2
+{
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+      string[] content = File.ReadAllLines("TextFile1.txt");
+      
+      bool valid = false;
+      int index = 0;
+      while (!valid)
+      {
+        var programCopy = (string[])content.Clone();
+         if (programCopy[index].StartsWith("nop"))
+          programCopy[index] = programCopy[index].Replace("nop", "jmp");
+        else if (programCopy[index].StartsWith("jmp"))
+          programCopy[index] = programCopy[index].Replace("jmp", "nop");
+        else
+        {
+          index++;
+          continue;
+        }
+
+        int acc = 0;
+        valid = true;
+        HashSet<int> program = new HashSet<int>();
+        for (int i = 0; i < programCopy.Length; i++)
+        {
+          string inst = programCopy[i];
+          if (!program.Add(i))
+          {
+            valid = false;
+            break;
+          }
+
+          string[] instruction = inst.Split(" ");
+          int arg = int.Parse(instruction[1]);
+          if (instruction[0] == "acc")
+            acc += arg;
+          if (instruction[0] == "jmp")
+            i += arg - 1;
+        }
+
+        index++;
+
+        if (valid)
+          Console.WriteLine(acc);
+      }
+    }
+  }
+}
