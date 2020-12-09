d0244d79783fd913af100a45f930dfa1ab4f7fcf
Alex Dragomir <alex.dragomir@caphyon.com>
2020-12-09 15:31:36 +0200

    9

---
+++ 9/9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace day2
+{
+  class Program
+  {
+
+    const int rangeSize = 25;
+
+    static void Main(string[] args)
+    {
+      var numbers = File.ReadAllLines("TextFile1.txt").Select(line => long.Parse(line)).ToList();
+
+      var invalid = numbers.Select((number, index) => new { number, index })
+        .ToList()
+        .GetRange(rangeSize, numbers.Count - rangeSize - 1)
+        .FirstOrDefault(num => !numbers.GetRange((int)num.index - rangeSize, rangeSize)
+          .Any(n => numbers.GetRange((int)num.index - rangeSize, rangeSize).
+            Any(rest => rest + n == num.number)));
+
+      foreach (var size in Enumerable.Range(2, rangeSize))
+      {
+        foreach (var index in Enumerable.Range(0, numbers.Count - rangeSize))
+        {
+          var r = numbers.GetRange(index, size);
+          if (r.Sum() == invalid.number)
+            Console.WriteLine(r.Min() + r.Max());
+        }
+      }
+    }
+  }
+}
