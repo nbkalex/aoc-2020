40a33f8a33aee3e45e8172eee2293a93ad8c2110
Alex Dragomir <alex.dragomir@caphyon.com>
2020-12-11 17:38:50 +0200

    fixed day 10

--- 10/10/Program.cs
+++ 10/10/Program.cs
@@ -19,13 +19,16 @@ namespace day2
         .OrderBy(n => n)
         .ToList();
 
-      var res = numbers.Skip(1).Zip(numbers).Select(n => n.First - n.Second)                                        // diffs                                                                           // as string            
-        .Aggregate(numbers.First().ToString(), (acc, n) => acc + n.ToString())                                      // as string
-        .Split('3')                                                                                                 // group of 1
-        .Where(g => g.Length > 1)                                                                                   // exclude irelevand groups
-        .Aggregate((long)1, (acc, g) => g.Length == 2 ? acc * 2 :  acc * (4 * (g.Length - 2) - (g.Length - 3)));    // compute value
+      var init = new int[] { 1, 2, 4 };
+      var perms = new int[10];
+      perms = perms.Select((p, i) => perms[i] = i < 3 ? init[i] : perms[i - 1] + perms[i - 2] + perms[i - 3]).ToArray();
 
-      Console.WriteLine(res);
+      Console.WriteLine(
+        numbers.Skip(1)
+               .Zip(numbers).Select(n => n.First - n.Second)                                        // diffs                 
+               .Aggregate(numbers.First().ToString(), (acc, n) => acc + n.ToString())               // as string
+               .Split('3', StringSplitOptions.RemoveEmptyEntries)                                   // group of 1
+               .Aggregate((long)1, (acc, g) => acc * perms[g.Length-1]));                           // compute value 
     }
   }
 }
