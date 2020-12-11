09902c0e1631d36b0506b5a4b3669995c8fad2ce
Alex Dragomir <alex.dragomir@caphyon.com>
2020-12-11 02:06:51 +0200

    10 simplified again

--- 10/10/Program.cs
+++ 10/10/Program.cs
@@ -19,23 +19,14 @@ namespace day2
         .OrderBy(n => n)
         .ToList();
 
-      var res = numbers.Select((n, i) => i < numbers.Count - 1 ? numbers[i + 1] - n : 3)
+      var res = numbers.Select((n, i) => i > 0 ? n - numbers[i-1] : n)
         .Select((n, i) => new { i, n })
-        .Aggregate(numbers.First().ToString(), (acc, n) => acc + n.n.ToString())
+        .Aggregate("", (acc, n) => acc + n.n.ToString())
         .Split('3')
         .Where(g => g.Length > 1)
         .Aggregate((long)1, (acc, g) => g.Length == 2 ? acc * 2 :  acc * (4 * (g.Length - 2) - (g.Length - 3)));
 
       Console.WriteLine(res);
-    }
-
-    static long Fact(int nr)
-    {
-      long res = 1;
-      foreach (var n in Enumerable.Range(2, nr))
-        res *= n;
-
-      return res;
     }
   }
 }
