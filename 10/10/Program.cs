16594a319d3351c24d1ca663c38470c01382b233
Alex Dragomir <alex.dragomir@caphyon.com>
2020-12-11 02:18:33 +0200

    10 explained

--- 10/10/Program.cs
+++ 10/10/Program.cs
@@ -15,16 +15,15 @@ namespace day2
     static void Main(string[] args)
     {
       var numbers = File.ReadAllLines("TextFile1.txt")
-        .Select(line => long.Parse(line))
+        .Select(line => int.Parse(line))
         .OrderBy(n => n)
         .ToList();
 
-      var res = numbers.Select((n, i) => i > 0 ? n - numbers[i-1] : n)
-        .Select((n, i) => new { i, n })
-        .Aggregate("", (acc, n) => acc + n.n.ToString())
-        .Split('3')
-        .Where(g => g.Length > 1)
-        .Aggregate((long)1, (acc, g) => g.Length == 2 ? acc * 2 :  acc * (4 * (g.Length - 2) - (g.Length - 3)));
+      var res = numbers.Select((n, i) => i > 0 ? n - numbers[i-1] : n)                                              // diffs                                                                           // as string            
+        .Aggregate("", (acc, n) => acc + n.ToString())                                                              // as string
+        .Split('3')                                                                                                 // group of 1
+        .Where(g => g.Length > 1)                                                                                   // exclude irelevand groups
+        .Aggregate((long)1, (acc, g) => g.Length == 2 ? acc * 2 :  acc * (4 * (g.Length - 2) - (g.Length - 3)));    // compute value
 
       Console.WriteLine(res);
     }
