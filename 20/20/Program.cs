b451fec011700ae4a6f74c580d8ac24925db2390
Alex Dragomir <alex.dragomir@caphyon.com>
2020-12-27 00:21:18 +0200

    20-1

--- 20/20/Program.cs
+++ 20/20/Program.cs
@@ -27,91 +27,27 @@ namespace day2
 
       }
 
-      Dictionary<int, int> rotation = tiles.ToDictionary(t => t.Key, t => 0);
-      Dictionary<int, HashSet<int>[]> neighbours = tiles.ToDictionary(t => t.Key, t => new HashSet<int>[]{ new HashSet<int>(),new HashSet<int>(),new HashSet<int>(),new HashSet<int>() }); // N S E V
 
-      foreach (var tile in tiles)
+      Dictionary<int, List<string>> tilesEdges = tiles.ToDictionary(tile => tile.Key, tile =>
+      new List<string>()
       {
-        foreach (var other in tiles)
-        {
-          if (tile.Key == other.Key)
-            continue;
+        tile.Value.First(),
+        new string(tile.Value.First().Reverse().ToArray()),
+        tile.Value.Last(),
+        new string(tile.Value.Last().Reverse().ToArray()),
+        new string(tile.Value.Select(t => t.First()).ToArray()),
+        new string(tile.Value.Select(t => t.First()).Reverse().ToArray()),
+        new string(tile.Value.Select(t => t.Last()).ToArray()),
+        new string(tile.Value.Select(t => t.Last()).Reverse().ToArray())
+      });
 
-          int match = GetMatch(tile.Value, other.Value);
-          if (match != -1)
-            neighbours[tile.Key][match].Add(other.Key);
+      var neighbours = tilesEdges.ToDictionary(te => te.Key,
+        te => tilesEdges.Where(te2 => te2.Key != te.Key && te2.Value.Intersect(te.Value).Any()).ToList());
 
-          match = GetMatch(tile.Value, RotateLeft(other.Value));
-          if (match != -1)
-            neighbours[tile.Key][match].Add(other.Key);
-        }
-      }
-
-      Console.WriteLine(string.Join(" \n",neighbours.Select(n => n.Key.ToString() + "-"+ n.Value.Count(v => v.Count != 0).ToString()).ToArray()));
-      Console.WriteLine(neighbours.Where(n => n.Value.Count(v => v.Count == 0) == 2).Aggregate((long)1, (acc, v) => acc * v.Key));
+      Console.WriteLine(neighbours.Where(n => n.Value.Count == 3).Aggregate((long)1, (acc, n) => acc * n.Key));
     }
 
-    static int GetMatch(List<string> t1, List<string> tile2)
-    {
-      //var tile1 = RotateRight(FlipHorizontal(t1));
-      var tile1 = t1;
-
-      if (tile1.First() == tile2.First() || tile1[0] == tile2.Last())
-        return 0;
-
-      if (tile1.First() == new string(tile2.First().Reverse().ToArray()) || tile1[0] == new string(tile2.Last().Reverse().ToArray())) //flip
-        return 0;
-
-      if (tile1.Last() == tile2.First() || tile1.Last() == tile2.Last())
-        return 1;
-
-      if (tile1.Last() == new string(tile2.First().Reverse().ToArray()) || tile1.Last() == new string(tile2.Last().Reverse().ToArray())) //flip
-        return 1;
-
-      var zip = tile1.Zip(tile2);
-      if (zip.All(z => z.First.First() == z.Second.First() || z.First.First() == z.Second.Last()))
-        return 2;
-
-      if (zip.All(z => z.First.Last() == z.Second.First() || z.First.Last() == z.Second.Last()))
-        return 3;
-
-      var zipFlip = tile1.Zip(tile2.ToArray().Reverse());
-      if (zipFlip.All(z => z.First.First() == z.Second.First() || z.First.First() == z.Second.Last()))
-        return 2;
-
-      if (zipFlip.All(z => z.First.Last() == z.Second.First() || z.First.Last() == z.Second.Last()))
-        return 3;
-
-      tile2 = RotateLeft(tile2);
-
-      if (tile1.First() == tile2.First() || tile1[0] == tile2.Last())
-        return 0;
-
-      if (tile1.First() == new string(tile2.First().Reverse().ToArray()) || tile1[0] == new string(tile2.Last().Reverse().ToArray())) //flip
-        return 0;
-
-      if (tile1.Last() == tile2.First() || tile1.Last() == tile2.Last())
-        return 1;
-
-      if (tile1.Last() == new string(tile2.First().Reverse().ToArray()) || tile1.Last() == new string(tile2.Last().Reverse().ToArray())) //flip
-        return 1;
-
-      zip = tile1.Zip(tile2);
-      if (zip.All(z => z.First.First() == z.Second.First() || z.First.First() == z.Second.Last()))
-        return 2;
-
-      if (zip.All(z => z.First.Last() == z.Second.First() || z.First.Last() == z.Second.Last()))
-        return 3;
-
-      zipFlip = tile1.Zip(tile2.ToArray().Reverse());
-      if (zipFlip.All(z => z.First.First() == z.Second.First() || z.First.First() == z.Second.Last()))
-        return 2;
-
-      if (zipFlip.All(z => z.First.Last() == z.Second.First() || z.First.Last() == z.Second.Last()))
-        return 3;
-
-      return -1;
-    }
+   
 
     static List<string> FlipVertical(List<string> tile)
     {
